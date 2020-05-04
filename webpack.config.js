a06c83c0f1dd5db6c4d938ec5fd0450ea976055f
aanchirinah <audax.anchirinah@andela.com>
2020-05-04 21:33:13 +0000

    - Get webpack working with Material UI - Configured Webpack config to work with css and image files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -6,6 +7,10 @@ module.exports = {
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
+    devServer: {
+        historyApiFallback: true,
+        port: 4500
+      },
     entry: {
         app: path.join(__dirname, 'src', 'index.tsx')
     },
@@ -13,7 +18,7 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx"]
+        extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"]
     },
 
     module: {
@@ -23,11 +28,22 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: "ts-loader"
+                        loader: "babel-loader"
                     }
                 ]
             },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            // css-loader to bundle all the css files into one file and style-loader to add
+            // all the styles  inside the style tag of the document
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+            },
+            // All output '.js' files will have any sourcemaps re-processed by
+            // 'source-map-loader'.
             {
                 enforce: "pre",
                 test: /\.js$/,
@@ -39,15 +55,13 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
-        new HtmlWebpackPlugin({
+    plugins: [new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'index.html')
-        })
-    ],
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
+        })],
+    // When importing a module whose path matches one of the following, just assume
+    // a corresponding global variable exists and use that instead. This is
+    // important because it allows us to avoid bundling all of our dependencies,
+    // which allows browsers to cache those libraries between builds.
     externals: {
         "react": "React",
         "react-dom": "ReactDOM"
