f015c585c3e33993212e3c454b17d3d84155e8d7
aanchirinah <audax.anchirinah@andela.com>
2020-05-10 20:11:17 +0000

    - Changed url redirection to blank page

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -35,11 +35,11 @@ export class NewsItem extends Component<NewsItemProps> {
         </div>
         <div className="card-content">
           <span className="card-title activator grey-text text-darken-4">{article.title}<i className="material-icons right">more_vert</i></span>
-     <p><a href={article.url}>Read Article Here</a></p>
+     <p><a href={article.url} target="_blank">Read Article Here</a></p>
          </div>
          <div className="card-reveal">
            <span className="card-title grey-text text-darken-4">{article.title}<i className="material-icons right">close</i></span>
-     <p>{article.content} <a href={article.url}>Read More</a></p>
+     <p>{article.content} <a href={article.url} target="_blank">Read More</a></p>
          </div>
        </div>
        </div>
